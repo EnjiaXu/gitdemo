0c7b1b4f77a677efab406ea421eab4a0ad657d52
Enjia Xu <enjiaxu@berkeley.edu>
2019-10-03 19:09:19 -0700

    R script file

---
+++ code/demo-script.R
@@ -0,0 +1,15 @@
+# ===================================================================
+# Title: Lab05 R script file
+# Description:
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Enjia Xu
+# Date: 10-03-2019
+# ===================================================================
+
+# packages
+library(dplyr)    # data wrangling
+library(ggplot2)  # graphics
+
